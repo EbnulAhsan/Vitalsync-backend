1580f1b497c80b5d6cf16898f2debb30893190ad
EbnulAhsan <ebnu1721@gmail.com>
2026-07-01 19:15:42 +0600

    all validation added

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,9 +1,20 @@
 import express from "express";
 import { AuthController } from "./auth.controller";
+import { validateRequest } from "../../middlewares/validateRequest";
+import { AuthValidation } from "./auth.validation";
 
 const router = express.Router();
 
-router.post("/register", AuthController.registerUser);
-router.post("/login", AuthController.loginUser);
+router.post(
+    "/register",
+    validateRequest(AuthValidation.registerValidation),
+    AuthController.registerUser
+);
+
+router.post(
+    "/login",
+    validateRequest(AuthValidation.loginValidation),
+    AuthController.loginUser
+);
 
 export default router;
